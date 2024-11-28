ad3d05640e80fbe8bd98c12d3bbbe4b22d329673
bif21 <bif2112@mail.ru>
2024-11-28 21:08:42 +0500

    Буба

--- app/web/views.py
+++ app/web/views.py
@@ -2,32 +2,40 @@ import requests
 from django.shortcuts import render
 from django.core.paginator import Paginator
 
-
 def index(request):
-    find_faq = None
+    find_faq = []
     search_query = request.GET.get('search', '')  # Получаем поисковый запрос
+    selected_category = request.GET.get('category', '')  # Получаем выбранную категорию
+    base_url = 'http://127.0.0.1:8228/api/'
+
+    if selected_category:
+        api_url = f'{base_url}{selected_category}/'
+    else:
+        api_url = base_url
+
+    print("Запрос к API:", api_url)
 
     try:
-        find_faq = requests.get('http://127.0.0.1:8228/api/').json()
+        response = requests.get(api_url)
+        response.raise_for_status()  # Поднимаем исключение для статусов 4xx/5xx
+        find_faq = response.json()
 
-        # Фильтруем FAQ по поисковому запросу
         if search_query:
             find_faq = [faq for faq in find_faq if
-                        search_query.lower() in faq['question'].lower() or search_query.lower() in faq[
-                            'answer'].lower()]
+                        search_query.lower() in faq['question'].lower() or search_query.lower() in faq['answer'].lower()]
 
-        # Пагинация
-        page_number = request.GET.get('page', 1)  # Номер страницы из GET-параметра
-        paginator = Paginator(find_faq, 20)  # Создаем объект Paginator с лимитом 20
-        current_page = paginator.get_page(page_number)  # Получаем текущую страницу
+        page_number = request.GET.get('page', 1)
+        paginator = Paginator(find_faq, 20)
+        current_page = paginator.get_page(page_number)
 
     except requests.exceptions.RequestException as e:
         print("Ошибка при запросе к API:", e)
 
     context = {
         'find_faq': current_page,
-        'search_query': search_query,  # Передаем поисковый запрос
-        'paginator': paginator,  # Передаем объект Paginator
+        'search_query': search_query,
+        'selected_category': selected_category,
+        'paginator': paginator,
     }
 
     return render(request, 'web/index.html', context)
